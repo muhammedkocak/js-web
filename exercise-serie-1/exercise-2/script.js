8b80a3a51142a03e2027fd5e940bb6105a2ab9e6
Muhammed <muhammedkocak.dev@gmail.com>
2019-03-18 14:45:01 +0100

    Calculator

--- exercise-serie-1/exercise-2/script.js
+++ exercise-serie-1/exercise-2/script.js
@@ -3,3 +3,56 @@ console.log("This script is linked properly!")
 
 
 //Write your JS code here...
+function addition() {
+    let field1=document.getElementById("num1").value;
+    var field2=document.getElementById("num2").value;
+
+
+    let result=parseFloat(field1)+parseFloat(field2);
+
+    if(!isNaN(result))
+    {
+        document.getElementById("answer").innerHTML="The answer is "+result;
+    }
+}
+
+
+
+function substraction() {
+    let field1=document.getElementById("num1").value;
+    var field2=document.getElementById("num2").value;
+
+    let result=parseFloat(field1)-parseFloat(field2);
+
+    if(!isNaN(result))
+    {
+        document.getElementById("answer").innerHTML="The answer is "+result;
+    }
+}
+
+
+
+function multiplication() {
+    let field1=document.getElementById("num1").value;
+    var field2=document.getElementById("num2").value;
+
+    let result=parseFloat(field1)*parseFloat(field2);
+
+    if(!isNaN(result))
+    {
+        document.getElementById("answer").innerHTML="The answer is "+result;
+    }
+}
+
+
+function division() {
+    let field1=document.getElementById("num1").value;
+    var field2=document.getElementById("num2").value;
+
+    let result=parseFloat(field1)/parseFloat(field2);
+
+    if(!isNaN(result))
+    {
+        document.getElementById("answer").innerHTML="The answer is "+result;
+    }
+}
