6e151a796351356da6e02b7f05d818114e0a708c
Muhammed <muhammedkocak.dev@gmail.com>
2019-03-18 12:13:07 +0100

    Color selector

--- exercise-serie-1/exercise-1/script.js
+++ exercise-serie-1/exercise-1/script.js
@@ -3,3 +3,18 @@ console.log("This script is linked properly!")
 
 
 //Write your JS code here...
+
+
+let div = document.getElementById("blackDiv");
+
+function blue() {
+        div.setAttribute("id" , "blueDiv");
+}
+
+function red() {
+    div.setAttribute("id" , "redDiv");
+}
+
+function green() {
+    div.setAttribute("id" , "greenDiv");
+}
