ae758aa21c36b6eb37a01ecb7caebaa27d6e62a2
Muhammed <muhammedkocak.dev@gmail.com>
2019-03-19 10:22:08 +0100

    Super Villain application form

--- exercise-serie-1/exercise-3/script.js
+++ exercise-serie-1/exercise-3/script.js
@@ -3,3 +3,27 @@ console.log("This script is linked properly!")
 
 
 //Write your JS code here...
+
+function form() {
+    event.preventDefault()
+    let field1=document.getElementById("name").value;
+    let field2=document.getElementById("power").value;
+    let field3=document.getElementById("motivation").value;
+    let field4=document.getElementById("plan").value;
+    
+
+    let result=(field1 + " - " + field2 + " - " + field3 + " - " + field4);
+    
+
+    {
+        document.getElementById("display2").innerHTML=result;
+    }
+}
+
+
+function reset() {
+    document.getElementById("form").reset();
+}
+
+let display =document.getElementById("display");
+display.addEventListener("click", form);
